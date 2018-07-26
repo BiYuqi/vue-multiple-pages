b1c058340a92ba80f61c2def1484ce593ac7e13f
biyuqi <biyuqiwan@163.com>
2018-07-27 00:20:21 +0800

    Update sytle

--- src/pages/index/App.vue
+++ src/pages/index/App.vue
@@ -11,6 +11,15 @@ export default {
 </script>
 
 <style>
+body, html{
+  width: 100%;
+  height: 100%;
+}
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
