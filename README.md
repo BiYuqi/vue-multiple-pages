a54be2802911f0675ef16a0f3d999d6b9a396bb8
loadingmore <biyuqiwan@163.com>
2018-10-30 16:04:30 +0800

    ci部署 && Update README

--- README.md
+++ README.md
@@ -1,30 +1,74 @@
-# multi
+# [vue-multiple-pages](https://github.com/BiYuqi/vue-multiple-pages)
 
-> A Vue.js project
+> vue2+vue-router+vuex 多页面配置实例
 
-## Build Setup
+## Setting
+```js
+// glob是webpack安装时依赖的一个第三方模块，还模块允许你使用 *等符号, 例如lib/*.js就是获取lib文件夹下的所有js后缀名的文件
+const glob = require('glob')
+// 页面模板
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// 取得相应的页面路径，因为之前的配置，所以是src文件夹下的pages文件夹
+const PAGE_PATH = path.resolve(__dirname, '../src/pages')
+
+/**
+* 多入口配置,通过glob模块读取pages文件夹下的所有对应文件夹下的js后缀文件
+* 如果该文件存在 那么就作为入口处理
+*/
+exports.entries = function () {
+  const entryFiles = glob.sync(PAGE_PATH + '/*/*.js')
+  const entry = {}
+  entryFiles.forEach((filePath) => {
+      const filename = filePath.match(/(\w+)\.js$/)[1]
+      entry[filename] = filePath
+  })
+  return entry
+}
+/**
+* 多页面输出配置
+* 与上面的多页面入口配置相同，读取pages文件夹下的对应的html后缀文件，然后放入数组中
+*/
+exports.htmlPlugin = function () {
+  const entryHtml = glob.sync(PAGE_PATH + '/*/*.html')
+  const arr = []
+  entryHtml.forEach((filePath) => {
+      const filename = filePath.match(/(\w+)\.html$/)[1]
+      let conf = {
+          // 模板来源
+          template: filePath,
+          // 文件名称
+          filename: filename + '.html',
+          // 页面模板需要加对应的js脚本，如果不加这行则每个页面都会引入所有的js脚本
+          chunks: ['manifest', 'vendor', filename],
+          inject: true
+      }
+      if (process.env.NODE_ENV === 'production') {
+          conf = merge(conf, {
+              minify: {
+                  removeComments: true,
+                  collapseWhitespace: true,
+                  removeAttributeQuotes: true
+              },
+              chunksSortMode: 'dependency'
+          })
+      }
+      arr.push(new HtmlWebpackPlugin(conf))
+  })
+  return arr
+}
+```
+## Usage
 
 ``` bash
-# install dependencies
+git clone https://github.com/BiYuqi/vue-multiple-pages.git
+
+cd vue-multiple-pages
+
 npm install
 
-# serve with hot reload at localhost:8080
 npm run dev
 
-# build for production with minification
 npm run build
-
-# build for production and view the bundle analyzer report
-npm run build --report
-
-# run unit tests
-npm run unit
-
-# run e2e tests
-npm run e2e
-
-# run all tests
-npm test
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- build/utils.js
+++ build/utils.js
@@ -11,6 +11,52 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 // 取得相应的页面路径，因为之前的配置，所以是src文件夹下的pages文件夹
 const PAGE_PATH = path.resolve(__dirname, '../src/pages')
 
+/**
+* 多入口配置,通过glob模块读取pages文件夹下的所有对应文件夹下的js后缀文件
+* 如果该文件存在 那么就作为入口处理
+*/
+exports.entries = function () {
+  const entryFiles = glob.sync(PAGE_PATH + '/*/*.js')
+  const entry = {}
+  entryFiles.forEach((filePath) => {
+      const filename = filePath.match(/(\w+)\.js$/)[1]
+      entry[filename] = filePath
+  })
+  return entry
+}
+/**
+* 多页面输出配置
+* 与上面的多页面入口配置相同，读取pages文件夹下的对应的html后缀文件，然后放入数组中
+*/
+exports.htmlPlugin = function () {
+  const entryHtml = glob.sync(PAGE_PATH + '/*/*.html')
+  const arr = []
+  entryHtml.forEach((filePath) => {
+      const filename = filePath.match(/(\w+)\.html$/)[1]
+      let conf = {
+          // 模板来源
+          template: filePath,
+          // 文件名称
+          filename: filename + '.html',
+          // 页面模板需要加对应的js脚本，如果不加这行则每个页面都会引入所有的js脚本
+          chunks: ['manifest', 'vendor', filename],
+          inject: true
+      }
+      if (process.env.NODE_ENV === 'production') {
+          conf = merge(conf, {
+              minify: {
+                  removeComments: true,
+                  collapseWhitespace: true,
+                  removeAttributeQuotes: true
+              },
+              chunksSortMode: 'dependency'
+          })
+      }
+      arr.push(new HtmlWebpackPlugin(conf))
+  })
+  return arr
+}
+
 exports.assetsPath = function (_path) {
   const assetsSubDirectory = process.env.NODE_ENV === 'production'
     ? config.build.assetsSubDirectory
@@ -107,49 +153,3 @@ exports.createNotifierCallback = () => {
   }
 }
 
-
-/**
-* 多入口配置,通过glob模块读取pages文件夹下的所有对应文件夹下的js后缀文件
-* 如果该文件存在 那么就作为入口处理
-*/
-exports.entries = function () {
-    const entryFiles = glob.sync(PAGE_PATH + '/*/*.js')
-    const entry = {}
-    entryFiles.forEach((filePath) => {
-        const filename = filePath.match(/(\w+)\.js$/)[1]
-        entry[filename] = filePath
-    })
-    return entry
-}
-/**
-* 多页面输出配置
-* 与上面的多页面入口配置相同，读取pages文件夹下的对应的html后缀文件，然后放入数组中
-*/
-exports.htmlPlugin = function () {
-    const entryHtml = glob.sync(PAGE_PATH + '/*/*.html')
-    const arr = []
-    entryHtml.forEach((filePath) => {
-        const filename = filePath.match(/(\w+)\.html$/)[1]
-        let conf = {
-            // 模板来源
-            template: filePath,
-            // 文件名称
-            filename: filename + '.html',
-            // 页面模板需要加对应的js脚本，如果不加这行则每个页面都会引入所有的js脚本
-            chunks: ['manifest', 'vendor', filename],
-            inject: true
-        }
-        if (process.env.NODE_ENV === 'production') {
-            conf = merge(conf, {
-                minify: {
-                    removeComments: true,
-                    collapseWhitespace: true,
-                    removeAttributeQuotes: true
-                },
-                chunksSortMode: 'dependency'
-            })
-        }
-        arr.push(new HtmlWebpackPlugin(conf))
-    })
-    return arr
-}
